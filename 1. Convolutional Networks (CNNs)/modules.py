a5d12568232d3ac30aac3d8cb87ec908e1bcf02a
Adnan Siddiquei <adnanshirik@gmail.com>
2024-10-29 22:13:09 +0000

    Added a ConvBlock2d module.

---
+++ 1. Convolutional Networks (CNNs)/modules.py
@@ -0,0 +1,46 @@
+import torch.nn as nn
+
+class ConvBlock2d(nn.Module):
+    """A simple 2D convolutional block.
+
+    Includes a convolutional kernel, a LayerNorm and an activation function.
+    
+    The purpose of the LayerNorm is to stabilise and improve training dynamics [1]. LayerNorm 
+    normalises a given to mean 0, standard deviation 1.
+    
+    [1] Ba, J. L., Kiros, J. R., & Hinton, G. E. (2016). Layer normalization [arXiv preprint 
+        arXiv:1607.06450]. arXiv. https://arxiv.org/abs/1607.06450
+    """
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        output_shape: tuple[int, int],
+        kernel_size: int = 3,
+        padding: int | None = None,
+        stride: int = 1,
+        dilation: int = 1,
+        act: nn.Module = nn.ReLU,
+    ):
+        super().__init__()
+        assert kernel_size % 2 != 0  # ensure kernek_size is odd
+
+        if padding is None:
+            # compute the padding if it was not passed in
+            padding = kernel_size // 2
+
+        self.conv_block = nn.Sequential(
+            nn.Conv2d(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=kernel_size,
+                padding=padding,
+                stride=stride,
+                dilation=dilation
+            ),
+            nn.LayerNorm((out_channels, *output_shape)),
+            act()
+        )
+
+    def forward(self, x):
+        return self.conv_block(x)

a9570f366911f09e26dbdbd638fe70bf74f4fa44
Adnan Siddiquei <adnanshirik@gmail.com>
2024-11-09 23:21:57 +0000

    Updated model to include the token embeddings and position embeddings. Also added some more docs. Added a count_model_size function as well.

--- 3. Transformers/3.1. BERT/models.py
+++ 3. Transformers/3.1. BERT/models.py
@@ -1,17 +1,46 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 from dlc.transformers.modules import TransformerEncoderLayer
 
 
 class Bert(nn.Module):
-    def __init__(self, embedding_dim: int, num_transformer_layers: int):
+    def __init__(
+        self,
+        embedding_dim: int,
+        num_transformer_layers: int,
+        vocab_size: int,
+        max_sequence_length: int,
+    ):
         super().__init__()
+
         self.embedding_dim = embedding_dim
+        self.num_transformer_layer = num_transformer_layers
+        self.vocab_size = vocab_size
+        self.max_sequence_length = max_sequence_length
 
         assert (
-            self.embedding_dim % 64 == 0
+            embedding_dim % 64 == 0
         ), '`embedding_dim` must be a multiple of 64. `num_heads` is computed as embedding_dim // 64.'
+
+        """
+        Create a set of token embeddings, which will be used to encode each word in the vocabulary.
+        These are initialised to random embeddings, which will move and be learned throughout training
+        to acquire some form of semantic meaning.
+        """
+        self.token_embeddings = nn.Embedding(
+            num_embeddings=vocab_size, embedding_dim=embedding_dim
+        )
+
+        """
+        Create a set of positional embeddings which will be added onto the inputs during the forward
+        pass, so that the model can learn some concept of position. Again, these are randomly
+        initialised embeddings and are learned during training.
+        """
+        self.position_embeddings = nn.Embedding(
+            num_embeddings=max_sequence_length, embedding_dim=embedding_dim
+        )
 
         self.transformer_layers = nn.ModuleList()
 
@@ -20,13 +49,73 @@ class Bert(nn.Module):
                 TransformerEncoderLayer(
                     embedding_dim=embedding_dim,
                     num_heads=embedding_dim // 64,
-                    ff_hidden_dim=embedding_dim,
+                    # the 4x multiplier, as per orig. implementation, Sec 3.3., Vaswani, A. (2017).
+                    ff_hidden_dim=embedding_dim * 4,
                     dropout=0.1,
                 )
             )
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        self.output_layer = nn.Linear(embedding_dim, vocab_size)
+
+    def forward(self, token_ids: torch.Tensor) -> torch.Tensor:
+        # token_ids.shape = (batch_size, sequence_length)
+        assert len(token_ids.shape) == 2
+
+        batch_size, sequence_length = token_ids.shape
+
+        """
+        First extract the embeddings for the given token_ids.
+            token_embeddings.shape = (batch_size, sequence_length, embedding_size)
+        """
+        token_embeddings = self.token_embeddings(token_ids)
+
+        """
+        Now we need to add on the position embeddings.
+        Extracting the embeddings is a slightly convoluted operation but it makes sense if you work
+        through it slowly. We just need to create the position_ids for 1 batch, then unsqueeze and
+        expand it so it is repeated for every batch.
+        """
+        position_ids = torch.arange(
+            sequence_length, dtype=torch.long, device=token_ids.device
+        )
+        position_ids = position_ids.unsqueeze(0).expand(batch_size, sequence_length)
+        position_embeddings = self.position_embeddings(position_ids)
+
+        """
+        And here we have the batch of inputs.
+            x.shape = (batch_size, sequence_length, embedding_size)
+        """
+        x = token_embeddings + position_embeddings
+
+        """
+        Now we pass the batch through the model.
+            Pass through every transformer layer.
+            Pass through the Linear output layer to change the embedding_dim -> vocab_size
+            Pass through softmax to get token probabilities.
+        """
         for layer in self.transformer_layers:
             x = layer(x)
 
-        return x
+        # logits are the raw, unnormalised probabilities that a given token is in a given class
+        logits = self.output_layer(x)
+        # logits.shape = (batch_size, sequence_length, vocab_size)
+
+        probabilities = F.softmax(logits, dim=-1)
+        # probabilities.shape = (batch_size, sequence_length, vocab_size)
+
+        return probabilities
+
+
+class BertBase(Bert):
+    def __init__(self):
+        embedding_dim = 768
+        num_transformer_layers = 12
+        vocab_size = 3000
+        max_sequence_length = 512
+
+        super().__init__(
+            embedding_dim=embedding_dim,
+            num_transformer_layers=num_transformer_layers,
+            vocab_size=vocab_size,
+            max_sequence_length=max_sequence_length,
+        )
--- dlc/utils.py
+++ dlc/utils.py
@@ -6,3 +6,12 @@ import numpy as np
 def count_parameters(model: nn.Module | L.LightningModule) -> int:
     """Calculate the number of parameters in a model."""
     return np.sum([p.numel() for p in model.parameters()])
+
+
+def count_model_size(model: nn.Module | L.LightningModule) -> int:
+    """Returns the model size in MB"""
+    model_size_bytes = np.sum(
+        [p.element_size() * p.numel() for p in model.parameters()]
+    )
+    model_size_mb = model_size_bytes / (1024**2)
+    return model_size_mb

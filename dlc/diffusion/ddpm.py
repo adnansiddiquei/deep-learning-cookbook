570eb2047ce012b1ca0f92b95aaace4b59a38d14
Adnan Siddiquei <adnanshirik@gmail.com>
2024-11-15 00:08:04 +0000

    Completed noise schedule computation.

--- dlc/diffusion/ddpm.py
+++ dlc/diffusion/ddpm.py
@@ -1,6 +1,6 @@
 """
 This file implements the Diffusion denoising probabilistic model (DDPM) as originaly defined by
-Ho, J., et al (2020).
+Denoising Diffusion Probabilistic Models Ho, J., et al (2020).
 
 The concepts explored here are explained in more detail in Ch. 18 of Understanding Deep Learning
 by Simon Prince.
@@ -11,7 +11,7 @@ A DDPM consists of an encoder and a decoder.
 
 The Encoder (forward process)
 -----------------------------
-Over a series of timesteps (e.g., 1000 steps), the encoder adds Gaussian noise to a given data
+Over a series of timesteps T (e.g., 1000 steps), the encoder adds Gaussian noise to a given data
 sample `x` (e.g., this could be a 3x256x256 RGB image) as well as attenuating the original signal.
 
 In this case, over 1000 steps, the encoder maps `x` through latent variables z_1, z_2, ..., z_1000.
@@ -37,19 +37,25 @@ So this means that you can have different times of "noising schedules". In the e
 a linear noise schedule.
 This simply means that beta_t ranges linearly between [beta_1, beta_2].
 
+So for example, if beta_t ranges linearly [1e-4, 0.2] with total timesteps T = 1000 (as chosen by
+Ho, J., et al (2020)), then
+    At t = 100      alpha_t = 0.3656, sqrt(alpha_t) = 0.6046
+                    which means that 60% of the data is signal, the rest is noise
+
 The Decoder (reverse process)
 -----------------------------
-This can be any model, but the objective of this model is to learn the noise (eps) added to the
-latent at any given step, to get to the next step.
+The decoder's objective is to learn the noise (eps) added to the latent variable at any given step,
+in order to reverse the process and reconstruct the original data sample x.
 
-The noise added to the latents at any given step is always scaled standard normal noise, so
-effectively the decoder needs to learn to predict this noise accurately in order to denoise the
-latent variables and reconstruct the original data sample `x`.
+During the forward process, noise is added to the data sample over a series of timesteps, resulting
+in a noisy latent variable. The decoder's task is to predict this noise accurately at each step,
+allowing it to denoise the latent variables and reconstruct the original data sample.
 
-So the decoder simply needs to predict standard normal noise at each step.
-
-
+The decoder receives a latent variable (composed of an attenuated signal and noise) and the current
+timestep as inputs. It must predict the specific noise that was added to the latent variable at the
+previous step, enabling the reconstruction of the original data sample.
 """
+
 import torch
 import torch.nn as nn
 
@@ -68,14 +74,24 @@ class DDPM(nn.Module):
 
         self.decoder = decoder
 
-    def _compute_noise_schedule(self, beta1, beta2, num_timesteps):
-        """
-
-        Noise added
+    def _compute_noise_schedule(
+        self, beta_1: float, beta_2: float, T: int
+    ) -> tuple[torch.Tensor, torch.Tensor]:
+        """Computes a linear noise schedule.
 
         Args:
-            beta1 (_type_): _description_
-            beta2 (_type_): _description_
-            num_timesteps (_type_): _description_
+            beta_1: Lower bound for noise schedule.
+            beta_2: Upper bound for noise schedule.
+            T: Total number of timesteps in the forward and reverse process.
         """
-        assert beta1 < beta2 < 1.0, 'beta1 and beta2 must be in (0, 1)'
+        assert 0.0 < beta_1 < beta_2 <= 1.0, 'beta1 and beta2 must be in [0, 1]'
+
+        # Create a linear schedule from `beta1` to `beta2` over `T` timesteps
+        steps = torch.arange(0, T + 1, dtype=torch.float32)
+        beta_t = (beta_2 - beta_1) * (steps / T) + beta_1
+
+        # Compute the corresponding `alpha_t` schedule (Sec 18.2.1, Eqn, 18.7; Prince)
+        # Cumprod in log-space (better precision) that simple prod
+        alpha_t = torch.exp(torch.cumsum(torch.log(1 - beta_t), dim=0))
+
+        return beta_t, alpha_t
